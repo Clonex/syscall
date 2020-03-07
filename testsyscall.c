fc557372ed4bed3f70948157a4b75e324cd13b27
Michael <clonex_kontakt@hotmail.com>
2020-03-07 17:13:44 +0100

    Updated print

--- testsyscall.c
+++ testsyscall.c
@@ -8,11 +8,11 @@ int main(int argc, char ** argv) {
 	printf("Calling ... \n");
     int length = sizeof(char);
     char temp = 'A';
-    printf("Input %s\n", &temp);
+    printf("Input %c\n", &temp);
 	syscall(__NR_dm510_msgbox_put, &temp, length);
 
     char* ret = malloc(length);
 	syscall(__NR_dm510_msgbox_get, ret, length);
 
-    printf("Ret %s\n", ret);
+    printf("Ret %c\n", ret);
 }
