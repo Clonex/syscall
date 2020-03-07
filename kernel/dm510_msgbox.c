419d06d7dedbdebc15c0335f646aec1373f59054
kberg18 <kberg18@student.sdu.dk>
2020-03-07 16:02:31 +0100

    Added exitcodes

--- kernel/dm510_msgbox.c
+++ kernel/dm510_msgbox.c
@@ -19,9 +19,9 @@ static msg_t *top = NULL;
 
 int dm510_msgbox_put( char *buffer, int length ){
 	if( length < 0 ){
-		return EINVAL; //Invalid argument
+		return -EINVAL; // Invalid argument
 	}
-	else if( access_ok(buffer, length) ){
+	if( access_ok(buffer, length) ){
 		msg_t* msg = kmalloc(sizeof(msg_t), GFP_KERNEL);
 
 		if( msg != NULL ){
@@ -29,8 +29,12 @@ int dm510_msgbox_put( char *buffer, int length ){
 			msg->length = length;
 			msg->message = kmalloc(length, GFP_KERNEL);
 
-			if( copy_from_user( msg->message, buffer, length ) == 0 ){ //copied everything
-				if(bottom == NULL) {
+			if( msg->message == NULL ){
+				return -ENOMEM; // Out of memory (the only case kmalloc returns NULL)
+			}
+
+			if( copy_from_user( msg->message, buffer, length ) == 0 ){ // Copied everything
+				if(bottom == NULL){
 					bottom = msg;
 					top = msg;
 				}
@@ -41,18 +45,21 @@ int dm510_msgbox_put( char *buffer, int length ){
 				}
 				return 0;
 			}
-
+			return -EAGAIN; // Try again
 		}
-
+		return -ENOMEM; // Out of memory (the only case kmalloc returns NULL)
 	}
-	return -14; //Bad address
+	return -EFAULT; // Bad address
 }
 
 int dm510_msgbox_get( char* buffer, int length ) {
 	if( length < 0 ){
-		return -22; //Invalid argument
+		return -EINVAL; // Invalid argument
 	}
-	else if( access_ok(buffer, length) && top != NULL ){
+	if( top == NULL ){
+		return -ENODATA; // No data available
+	}
+	if( access_ok(buffer, length) ){
 		msg_t* msg = top;
 		char *tmp = msg->message;
 		int mlength = msg->length;
@@ -68,9 +75,9 @@ int dm510_msgbox_get( char* buffer, int length ) {
 
 			return mlength;
 		}
-		return 100; //TODO: Find accurate exit code
+		return -EAGAIN; // Try again
 	}
-	return -14; //Bad address
+	return -EFAULT; // Bad address
 }
 
 int main ( void ) {
