233232d1ea9c7793c1e612dc7b4a144a82b5ca1c
Michael <clonex_kontakt@hotmail.com>
2020-03-07 17:04:13 +0100

    Updated movefiles

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,5 +1,6 @@
 cp syscall_64.tbl ../linux-5.6-rc1/arch/x86/entry/syscalls/syscall_64.tbl
 cp unistd_64.h ../linux-5.6-rc1/arch/x86/include/generated/uapi/asm/unistd_64.h
+cp testsyscall.c ../linux-5.6-rc1/testsyscall.c
 
 cp -a headers/. ../linux-5.6-rc1/arch/um/include/asm
 cp -a kernel/. ../linux-5.6-rc1/arch/um/kernel/
