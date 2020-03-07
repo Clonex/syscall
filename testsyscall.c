51ff652aa408d070494dc43ffcbe05f789d87f6a
Michael <clonex_kontakt@hotmail.com>
2020-03-07 17:07:52 +0100

    Fixed testSysCall file

--- testsyscall.c
+++ testsyscall.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <errno.h>
+#include <stdlib.h> 
 #include <unistd.h>
 #include "arch/x86/include/generated/uapi/asm/unistd_64.h"
 
@@ -7,11 +8,11 @@ int main(int argc, char ** argv) {
 	printf("Calling ... \n");
     int length = sizeof(char);
     char temp = 'A';
-    printf("Input %s\n", temp);
+    printf("Input %s\n", &temp);
 	syscall(__NR_dm510_msgbox_put, &temp, length);
 
     char* ret = malloc(length);
-	syscall(dm510_msgbox_get, ret, length);
+	syscall(__NR_dm510_msgbox_get, ret, length);
 
-    printf("Ret %s\n", *ret);
+    printf("Ret %s\n", ret);
 }
