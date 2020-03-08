f98670316039265b23776112a9c6624370637ad0
Michael <clonex_kontakt@hotmail.com>
2020-03-08 13:55:57 +0100

    Filename typo

--- moveFiles.sh
+++ moveFiles.sh
@@ -2,7 +2,7 @@ cp syscall_64.tbl ../linux-5.6-rc1/arch/x86/entry/syscalls/syscall_64.tbl
 cp unistd_64.h ../linux-5.6-rc1/arch/x86/include/generated/uapi/asm/unistd_64.h
 cp testsyscall.c ../linux-5.6-rc1/testsyscall.c
 cp spawnThreads.sh ../linux-5.6-rc1/spawnThreads.sh
-cp kernelMakefile ../linux-5.6-rc1/arch/um/kernel/Makefile
+cp kernel-Makefile ../linux-5.6-rc1/arch/um/kernel/Makefile
 
 cp -a headers/. ../linux-5.6-rc1/arch/um/include/asm
 cp -a kernel/. ../linux-5.6-rc1/arch/um/kernel/
