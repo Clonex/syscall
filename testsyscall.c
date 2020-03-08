75fd1139cc9dfc2105b4eafc7080e85b3ea67837
Michael <clonex_kontakt@hotmail.com>
2020-03-08 14:14:27 +0100

    Tests working

--- testsyscall.c
+++ testsyscall.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h> 
+#include <string.h>
 #include <unistd.h>
 #include "arch/x86/include/generated/uapi/asm/unistd_64.h"
 
 int main(int argc, char ** argv) {
 	printf("Running tests!\n");
-	char str[50] = "This is a stupid message.";
+	char str[] = "This is a stupid message.";
 	int length = sizeof(str);
 
-	int inputSize = 10;
-	syscall(__NR_dm510_msgbox_put, str, inputSize);
+	syscall(__NR_dm510_msgbox_put, str, length);
 
 	char* ret = malloc(length);
-	syscall(__NR_dm510_msgbox_get, ret, 25);
-	printf("Input 0: %s\n", ret);
-	printf("%s\n", ret == "This is a stupid message." ? "\033[0;32mPASSED\033[0m;" : "FAILED");
+	syscall(__NR_dm510_msgbox_get, ret, length);
+	printf("Input 0: %s\n", strcmp(ret, "This is a stupid message.") == 0 ? "\033[0;32mPASSED\033[0m;" : "\033[0;31mFAILED\033[0m;");
 	free(ret);
 
-	//printf("Testing input order! \n");
-
-
 	int cLength = sizeof(char);
-	syscall(__NR_dm510_msgbox_put, 'A', cLength);
-	syscall(__NR_dm510_msgbox_put, 'B', cLength);
-	syscall(__NR_dm510_msgbox_put, 'C', cLength);
+	char temp = 'A';
+	syscall(__NR_dm510_msgbox_put, &temp, cLength);
+	temp = 'B';
+	syscall(__NR_dm510_msgbox_put, &temp, cLength);
+	temp = 'C';
+	syscall(__NR_dm510_msgbox_put, &temp, cLength);
 
 	ret = malloc(length);
-	syscall(__NR_dm510_msgbox_get, ret, 25);
+	syscall(__NR_dm510_msgbox_get, ret, cLength);
+	printf("Input 1 %s\n", *ret == 'C' ? "\033[0;32mPASSED\033[0m;" : "\033[0;31mFAILED\033[0m;"));
 
-	printf("Input 1 %s\n", *ret == 'A' ? "PASSED" : "FAILED");
-	printf("Input 2 %s\n", *ret == 'B' ? "PASSED" : "FAILED");
-	printf("Input 3 %s\n", *ret == 'C' ? "PASSED" : "FAILED");
+	syscall(__NR_dm510_msgbox_get, ret, cLength);
+	printf("Input 2 %s\n", *ret == 'B' ? "\033[0;32mPASSED\033[0m;" : "\033[0;31mFAILED\033[0m;"));
+
+	syscall(__NR_dm510_msgbox_get, ret, cLength);
+	printf("Input 3 %s\n", *ret == 'A' ? "\033[0;32mPASSED\033[0m;" : "\033[0;31mFAILED\033[0m;"));
 	free(ret);
 }
