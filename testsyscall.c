0a91fcfa75dd01c2220db423eb36aeffb02f601b
Kasper Bergstedt <kberg18@student.sdu.dk>
2020-03-08 10:14:50 +0100

    changed testfile

--- testsyscall.c
+++ testsyscall.c
@@ -8,11 +8,11 @@ int main(int argc, char ** argv) {
 	printf("Testing ... \n");
     char str[50] = "This is a stupid message.";
     int length = sizeof(str);
-    printf("Input: %c\n", temp);
+    printf("Input: %s\n", str);
     syscall(__NR_dm510_msgbox_put, str, length);
 
     char* ret = malloc(length);
-    syscall(__NR_dm510_msgbox_get, ret, length);
+    syscall(__NR_dm510_msgbox_get, ret, 10);
 
-    printf("Output: %c\n", *ret);
+    printf("Output: %s\n", ret);
 }
